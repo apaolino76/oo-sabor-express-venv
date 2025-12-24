1025694dda2837bf2bc3c9a65f651b85e6bcc88b
angelo.paolino <angelo.paolino@dataprev.gov.br>
2025-12-24 09:34:58 -0300

    correcao de um bug no arquivo app.py

--- app.py
+++ app.py
@@ -19,18 +19,18 @@ if response.status_code == 200:
             'price': item['price'],
             'description': item['description']
         })
+
+    # 1. Garante que a pasta existe
+    pasta_destino = "arqs"
+    os.makedirs(pasta_destino, exist_ok=True)
+
+    for nome_do_restaurante, dados in dados_restaurantes.items():
+        nome_do_arquivo = f'{nome_do_restaurante}.json'
+        caminho_arquivo = os.path.join(pasta_destino, nome_do_arquivo)
+
+        with open(caminho_arquivo, 'w+') as arquivo_restaurante:
+            json.dump(dados, arquivo_restaurante, indent=4)
 else:
     print('f O erro foi {response.status_code}')
 
 # print(dados_restaurantes['McDonald’s'])
-
-# 1. Garante que a pasta existe
-pasta_destino = "arqs"
-os.makedirs(pasta_destino, exist_ok=True)
-
-for nome_do_restaurante, dados in dados_restaurantes.items():
-    nome_do_arquivo = f'{nome_do_restaurante}.json'
-    caminho_arquivo = os.path.join(pasta_destino, nome_do_arquivo)
-
-    with open(caminho_arquivo, 'w+') as arquivo_restaurante:
-        json.dump(dados, arquivo_restaurante, indent=4)
